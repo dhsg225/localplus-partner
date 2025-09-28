3f9630cbeb6c0606ddf0701edea12690517896de
dhsg225 <shannon.green.asia@gmail.com>
2025-09-28 11:26:18 +0700

    Fix Tailwind config to be self-contained

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
-const sharedConfig = require('../tailwind.config.js');
-
-module.exports = {
-  ...sharedConfig,
+/** @type {import('tailwindcss').Config} */
+export default {
   content: [
     './index.html',
     './src/**/*.{js,ts,jsx,tsx}',
-    '../shared/**/*.{js,ts,jsx,tsx}',
   ],
-};
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
